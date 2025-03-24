151258153c4b25f150bf10d159a0673e8035ead8
Prateek Jain <jprateek601@gmail.com>
2025-03-24 14:59:26 +0530

    feat: starting vice animation project

--- lib/app/constants/constants.dart
+++ lib/app/constants/constants.dart
@@ -1,16 +1,21 @@
 import 'package:animation_samples/app/models/project.dart';
+import 'package:animation_samples/app/routes.dart';
 
 List<Project> projects = [
   Project(
       title: "Card Hover Animation",
       description: "See the beautiful hover animation",
-      route: "/card_hover"),
+      route: RouteName.cardHover),
   Project(
       title: "Endless scrolling",
       description: "See the endless scrolling in motion",
-      route: "/endless-scrolling"),
+      route: RouteName.endlessScrolling),
   Project(
       title: "Radar animation",
       description: "check out the radial gradient used to make this animation",
-      route: "/radar-animation"),
+      route:RouteName.radarAnimation),
+  Project(
+      title: "Vice Slide Animation",
+      description: "Stunning slide animations",
+      route: RouteName.viceSlideAnimation),
 ];
--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -2,19 +2,31 @@ import 'package:animation_samples/app/home_screen.dart';
 import 'package:animation_samples/projects/card_hover_animation/card_hover_screen.dart';
 import 'package:animation_samples/projects/endless_scrolling_tutorial/endless_scrolling_screen.dart';
 import 'package:animation_samples/projects/radar_animation/radar_animation_screen.dart';
+import 'package:animation_samples/projects/vice_slide_animations/features/home/presentation/screens/vice_screen.dart';
 import 'package:flutter/material.dart';
 
 Route routes(RouteSettings settings) {
   switch (settings.name) {
-    case '/':
+    case RouteName.home:
       return MaterialPageRoute(builder: (_) => HomeScreen());
-    case '/card_hover':
+    case RouteName.cardHover:
       return MaterialPageRoute(builder: (_) => CardHoverScreen());
-    case '/endless-scrolling':
+    case RouteName.endlessScrolling:
       return MaterialPageRoute(builder: (_) => EndlessScrollingScreen());
-    case '/radar-animation':
+    case RouteName.radarAnimation:
       return MaterialPageRoute(builder: (_) => RadarAnimationScreen());
+    case RouteName.viceSlideAnimation:
+      return MaterialPageRoute(builder: (_) => ViceScreen());
     default:
       return MaterialPageRoute(builder: (_) => HomeScreen());
   }
+}
+
+class RouteName {
+  RouteName._();
+  static const String home = '/';
+  static const String cardHover = '/card_hover';
+  static const String endlessScrolling = '/endless-scrolling';
+  static const String radarAnimation = '/radar-animation';
+  static const String viceSlideAnimation = '/vice-slide-animation';
 }
